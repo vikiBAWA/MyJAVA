c92c416b3b3dd8aec62ae54c0c0faf4a5f4e7f0e
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-09-22 17:50:15 +0530

    Add files via upload

---
+++ Lab Practical/3rd Practical/Average calculate/PopulationAvg.java
@@ -0,0 +1,14 @@
+package mathsAverage;
+
+public class PopulationAvg {
+	public static void main(String args[])
+	{
+		int a = 262500;
+		int b = 175000;
+		int c = a - b ;
+		int d = (c*100) / 175000;
+		int e = d/10;
+		System.out.println("Average increase is: " + e + "%");
+	}
+
+}

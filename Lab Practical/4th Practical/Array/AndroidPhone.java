7ce6161b2e95a405b3ec4b99249066a8c2a724a3
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-09-29 17:26:57 +0530

    Add files via upload

---
+++ Lab Practical/4th Practical/Array/AndroidPhone.java
@@ -0,0 +1,28 @@
+package arrayprac;
+
+public class AndroidPhone {
+	
+	String name;
+	double display;
+	int ram;
+	String color;
+	int version;
+	int rom;
+	
+	public AndroidPhone(String name, double display, int ram, String color,int version, int rom) {
+		this.name = name;
+		this.display = display;
+		this.ram = ram;
+		this.color = color;
+		this.version = version;
+		this.rom = rom;
+	}
+
+	/*@Override
+	public String toString() {
+		return "AndroidPhone [name=" + name + ", display=" + display + ", ram=" + ram + ", color=" + color
+				+ ", version=" + version + ", rom=" + rom + "]";
+	}*/
+
+	
+}

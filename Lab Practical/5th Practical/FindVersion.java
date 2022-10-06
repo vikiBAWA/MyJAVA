182c61a9e3761da48d46a902967d47cbf9a65575
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-10-06 12:13:51 +0530

    Add files via upload

---
+++ Lab Practical/5th Practical/FindVersion.java
@@ -0,0 +1,8 @@
+package merajava;
+
+public class FindVersion {
+	public static final void main(String[] args)
+	{
+		System.out.println("Version of java is : " + System.getProperty("java.version"));
+	}
+}

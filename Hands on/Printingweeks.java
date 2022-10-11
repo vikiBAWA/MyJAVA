f2eacc874876a5448547341b8b5fde8dbfe9b63b
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-10-11 15:15:00 +0530

    Add files via upload

---
+++ Hands on/Printingweeks.java
@@ -0,0 +1,40 @@
+/*Program to Demonstrate (Switch) and printing week days
+  Name: Vikram Shinde
+  Date: 11.10.22
+*/
+package printingweeks;
+
+import java.util.Scanner;
+
+public class Printingweeks {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Week number: ");
+		int Case = sc.nextInt();
+		sc.close();
+        switch(Case)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Weekend");
+                break;
+        }
+    }
+	}
+
+

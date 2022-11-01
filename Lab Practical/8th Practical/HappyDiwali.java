ead6187ec84cecf8786841270ebbd97f4ed2aee0
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-11-01 18:21:40 +0530

    Add files via upload

---
+++ Lab Practical/8th Practical/HappyDiwali.java
@@ -0,0 +1,24 @@
+/*
+ Write a program to print Happy Diwali using any loop
+ Name: Vikram Shinde
+ Date:- 27/10/22
+*/
+package diwali;
+
+public class HappyDiwali {
+
+	public static void main(String[] args) {
+		//declaring the value string 
+		String a = "WISHING YOU A HAPPY DIWALI";
+		
+		//using for loop for string
+		for (int i=0;i<a.length();)
+			
+		{	
+			System.out.println(a);
+			//break statement to exit the loop after printing the string
+		break;
+		}
+	}
+
+}

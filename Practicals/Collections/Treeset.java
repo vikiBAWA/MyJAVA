456a1034e898ceda5a4126eefdfc1aa64df7f458
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-11-01 13:19:54 +0530

    Add files via upload

---
+++ Practicals/Collections/Treeset.java
@@ -0,0 +1,22 @@
+package array;
+import java.util.*;
+class Treeset {
+
+	public static void main(String[] args) {
+
+		SortedSet<String> fruits = new TreeSet<>();
+		  
+		  fruits.add("Mango");
+		  fruits.add("Banana");
+		  fruits.add("Apple");
+		  fruits.add("Pineapple");
+		  
+		  System.out.println("Fruits are : " +fruits);
+		  
+		  //duplicate elements are ignored
+		  fruits.add("Grapes");
+		  System.out.println();
+		  System.out.println("After adding duplicate elements : " +fruits);
+
+		 }
+}

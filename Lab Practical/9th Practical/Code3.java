5d410b68476236bfffe18ca7f9ec9c2ce2ef6f1b
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-11-04 10:58:45 +0530

    Update Code3.java

--- Lab Practical/9th Practical/Code3.java
+++ Lab Practical/9th Practical/Code3.java
@@ -16,14 +16,15 @@ public class Code3 {
 		Cars_1.add("AUDI");
 		Cars_1.add("MERCEDES");
 		Cars_1.add("ROLLS ROYCE");
+		//Printing First Set
 	    System.out.println("First Cars Set content: "+Cars_1);
-	          
+	    // use add() method to add values 
 	          HashSet<String>Cars_2 = new HashSet<String>();
 	          Cars_2.add("AUDI");
 	          Cars_2.add("MCLAREN");
 	          Cars_2.add("MUSTANG");
 	          Cars_2.add("LINCOLN");
-	  		  
+	  		  //Printing Second Set
 	          System.out.println("Second Cars Set content: "+Cars_2);
 	          Cars_1.retainAll(Cars_2);
 	          System.out.print("Common Elements : ");

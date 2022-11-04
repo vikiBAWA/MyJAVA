fe2572a425c1907187f041b04e3250d05e5c56a0
vikiBAWA <112938064+vikiBAWA@users.noreply.github.com>
2022-11-04 13:19:55 +0530

    Add files via upload

---
+++ Hands on/Bound.java
@@ -0,0 +1,12 @@
+package diyaapp;
+//import  java.util.*;
+
+//array index out of bound
+ class Bound {
+	public static void main(String[] args)
+	{
+		int ar[] = { 11, 22, 33, 44, 55 };
+		for (int i = 0; i <= ar.length; i++)
+			System.out.println(ar[i]);
+	}
+}
---
+++ Hands on/Example.java
@@ -0,0 +1,26 @@
+package diyaapp;
+import  java.util.*;
+
+class Example
+{
+	public static void main (String[] args)
+	{
+		// Initializing String variable with null value
+		String NPE = null;
+
+		// Checking if NPE.equals null or works fine.
+		try
+		{
+			// This line of code throws NullPointerException
+			// because NPE is null
+			if (NPE.equals("Vik"))
+				System.out.print("Same");
+			else
+				System.out.print("Not Same");
+		}
+		catch(NullPointerException a)
+		{
+			System.out.print("Null Pointer Exception Caught");
+		}
+	}
+}
